9c9c8aee7a6cc3778212ae119b405d919e14a703
SKIV-2611 <didm.platonov@yandex.ru>
2021-08-19 09:03:34 +0300

    Добавьте файлы проекта.

---
+++ Controllers/PaymentMockController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MbDotNet;
+using MbDotNet.Enums;
+using MbDotNet.Exceptions;
+using MbDotNet.Models;
+using MbDotNet.Models.Imposters;
+using MbDotNet.Models.Predicates;
+using MbDotNet.Models.Predicates.Fields;
+using MbDotNet.Models.Responses.Fields;
+using MbDotNet.Models.Stubs;
+using MbDotNet.Models.Responses;
+using MockApi.Models;
+
+namespace MockApi.Controllers
+{
+    [Route("api/PaymentMock")]
+    [ApiController]
+    public class PaymentMockController : ControllerBase
+    {
+        MountebankClient _client;
+        HttpImposter _imposter;
+        public PaymentMockController() : base()
+        {
+            _client = new MountebankClient();
+            _imposter = _client.CreateHttpImposter(8090);
+            _client.Submit(_imposter);
+        }
+
+        [HttpPost]
+        public async Task ReturnOrForward(PaymentDTO pDTO)
+        {
+
+            var complexPredicateF = new HttpPredicateFields
+            {
+                Method = Method.Post,
+                Path = "/api/PaymetMock",
+                Headers = new Dictionary<string, string>{ { "Accept", "application/json" } }
+            };
+            var complexPredicate = new EqualsPredicate<HttpPredicateFields>(complexPredicateF);
+            var predicateFields = new HttpPredicateFields
+            {
+                RequestBody = "!test"
+            };
+            _imposter.AddStub()
+                .On(new ContainsPredicate<HttpPredicateFields>(predicateFields))
+                .ReturnsStatus(System.Net.HttpStatusCode.OK)
+                .On(complexPredicate);
+
+        }
+    }
+}
---
+++ Models/PaymentDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockApi.Models
+{
+    public class PaymentDTO
+    {
+        public int ID { get; set; }
+        public int DboID { get; set; }
+
+        public string PayerAccountNumber { get; set; }
+
+        public string ReceiverAccountNumber { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string PaymentDetails { get; set; }
+    }
+}
